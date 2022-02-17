e6e31fd0ca51fd2fb37c5901f1b003f8b4cd516d
Julian Javier Pimeinta Rendon <jpimientarm56@gmail.com>
2022-02-16 18:00:57 -0700

    Committing addition of products in cold/hot drinks and sweets/salties As well as src for the images used

--- app/src/main/java/pimienta/julian/thecheezery/MenuActivity.kt
+++ app/src/main/java/pimienta/julian/thecheezery/MenuActivity.kt
@@ -1,11 +1,42 @@
 package pimienta.julian.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("key",1)
+            startActivity(intent)
+        }
+
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        btnHot.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("key",2)
+            startActivity(intent)
+        }
+
+        var btnSweet: Button = findViewById(R.id.button_sweets) as Button
+        btnSweet.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("key",3)
+            startActivity(intent)
+        }
+
+        var btnSalt: Button = findViewById(R.id.button_salties) as Button
+        btnSalt.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("key",4)
+            startActivity(intent)
+        }
+
     }
 }
